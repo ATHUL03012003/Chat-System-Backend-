5057869fac670ccafeb389875b9452862b692076
ATHUL03012003 <athulkrishnas2020@gmail.com>
2025-12-04 14:48:15 +0530

    feat/authentication

--- accounts/views.py
+++ accounts/views.py
@@ -6,3 +6,10 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
+
+class MeView(generics.RetrieveAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
